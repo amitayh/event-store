6b2144060700b2869017838552edfdec72f88318
Amitay Horwitz <amitayh@gmail.com>
2018-01-12 16:45:24 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cassandra-event-store
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cassandra_event_store/core_test.clj
@@ -0,0 +1,7 @@
+(ns cassandra-event-store.core-test
+  (:require [clojure.test :refer :all]
+            [cassandra-event-store.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
